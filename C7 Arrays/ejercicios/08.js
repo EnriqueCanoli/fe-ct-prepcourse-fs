55fa6b2e5e7567cba881c1cffd9039f704f561c4
EnriqueCanoli <enriquecanoli96@gmail.com>
2024-03-07 14:27:27 -0600

    limpieza codigo

--- C7 Arrays/ejercicios/08.js
+++ C7 Arrays/ejercicios/08.js
@@ -3,11 +3,6 @@ function encontrarElemento(num, array) {
    // Si lo encuentras debes retornar el INDICE en el que se encuentra dentro del array.
    // Si no se encuentra, retorna -1.
    // Tu código:
-   /*for(let i=0 ; i < array.length; i++){
-      if(array[i] == num) return i;
-   }
-
-   return -1;*/
 
    return array.indexOf(num);
 }
